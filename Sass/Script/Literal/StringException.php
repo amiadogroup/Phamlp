2ae72c200da051650eec1959cd8618a4e0ff9535
Jonatan Männchen <jonatan.maennchen@amiadogroup.com>
2012-02-02 20:56:24 +0100

    splited literal exceptions

---
+++ Sass/Script/Literal/StringException.php
@@ -0,0 +1,17 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Sass literal exception classes.
+ * @author			Chris Yates <chris.l.yates@gmail.com>
+ * @copyright 	Copyright (c) 2010 PBM Web Development
+ * @license			http://phamlp.googlecode.com/files/license.txt
+ * @package			PHamlP
+ * @subpackage	Sass.script.literals
+ */
+
+/**
+ * Phamlp_Sass_Script_Literal_StringException class.
+ * @package			PHamlP
+ * @subpackage	Sass.script.literals
+ */
+class Phamlp_Sass_Script_Literal_StringException extends Phamlp_Sass_Script_LiteralException {}
